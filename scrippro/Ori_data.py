8c9f4917a1c1df1e8bc27726c7725edd963fd7cd
xuyunfan9991 <xuyunfan9991@gmail.com>
2023-08-29 13:10:55 +0800

    init

---
+++ scrippro/Ori_data.py
@@ -0,0 +1,269 @@
+import math
+import multiprocessing
+import random
+import warnings
+from collections import Counter
+from multiprocessing import Pool
+from itertools import islice
+import anndata as ad
+import math
+import h5py
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import seaborn as sns
+from lisa import FromGenes
+from supercell import *
+from utils import *
+from functools import partial
+from scipy.stats import kendalltau, pearsonr, spearmanr
+class Ori_Data():
+    def __init__(self,adata,Cell_num):
+        self.adata = adata[:, ~adata.var_names.str.contains('\.|\-')]
+        self.adata_super = self.adata.copy()
+        self.Cell_num = Cell_num
+        self._fliter()
+        self._supercell()
+        self.super_gene_exp = self.ad_all.copy()
+        self.super_gene_mean = self.super_gene_exp.mean()
+        self.super_gene_std = self.super_gene_exp.std()
+        sc.tl.rank_genes_groups(self.adata,'new_leiden', method='t-test')
+        self.cellgroup = pd.DataFrame(self.adata.obs.iloc[:,-1])
+
+    def _spatialcluster(self,Target_sum=1e4,N_top_genes=3000,Rad_cutoff=50,Resolution=0.8):
+        sc.pp.normalize_total(self.adata, target_sum=Target_sum)
+        sc.pp.log1p(self.adata)
+        sc.pp.highly_variable_genes(self.adata,n_top_genes=N_top_genes)
+        STAGATE.Cal_Spatial_Net(self.adata, rad_cutoff=Rad_cutoff)
+        # STAGATE.Stats_Spatial_Net(adata)
+        self.adata = STAGATE.train_STAGATE(self.adata, alpha=0.5)
+        sc.pp.neighbors(self.adata, use_rep='STAGATE')
+        sc.tl.umap(self.adata)
+        sc.tl.leiden(self.adata, resolution=Resolution)
+        sc.pl.embedding(self.adata, basis="spatial", color=['leiden'],s=6, show=False, title='STAGATE',save="Spatial_cluster.pdf")
+        sc.pl.umap(self.adata, color=['leiden'],show=False,save="Spatial_umap_leiden.pdf")
+    
+    def _fliter(self,Target_sum=1e4,N_top_genes=3000,Max_value=10,N_neighbors=10,N_pcs=40,Resolution=0.6):
+        sc.pp.normalize_total(self.adata, target_sum=Target_sum)
+        sc.pp.log1p(self.adata)
+        sc.pp.highly_variable_genes(self.adata,n_top_genes=N_top_genes)
+        self.adata.raw = self.adata
+        self.adata = self.adata[:, self.adata.var.highly_variable]
+        #sc.pp.regress_out(self.adata, ['total_counts', 'pct_counts_mt'])
+        sc.pp.scale(self.adata, max_value=Max_value)
+        sc.tl.pca(self.adata,svd_solver='arpack')
+        sc.pp.neighbors(self.adata, n_neighbors=N_neighbors, n_pcs=N_pcs)
+        sc.tl.umap(self.adata)
+        sc.tl.leiden(self.adata,resolution=Resolution)
+ 
+    def get_positive_marker_gene_parallel(self, log2fc=0.5, pval=0.1, gene_list_len=20,cores=2):
+        pool = mp.Pool(cores)
+        groups = set(self.adata.obs['new_leiden'])
+        func = partial(get_marker_for_group, self.adata, log2fc=log2fc, pval=pval, gene_list_len=gene_list_len)
+        results = pool.map(func, groups)
+        pool.close()
+        self.marker_list = {i: tem_gene for i, tem_gene in results if tem_gene is not None}
+        
+    def _supercell(self):
+        adList = []
+        adList_obs = {}
+        for i in set(self.adata.obs.leiden):
+            leiden_index = self.adata.obs.loc[self.adata.obs.leiden == i].index
+            sub_cluster = self.adata_super[leiden_index]
+            merged_data,obs = supercell_pipeline(sub_cluster,cell_num=self.Cell_num,verbose=False)
+            merged_data_index = [i + "_" + str(j) for j in range(0,merged_data.shape[0])]
+            merged_data.obs = pd.DataFrame(index = merged_data_index)
+            adList.append(merged_data)
+            adList_obs[i] = obs
+        ad_all = ad.concat(adList,join='outer')
+        sc.pp.normalize_total(ad_all, target_sum=1e4)
+        sc.pp.log1p(ad_all)
+        ad_all = ad_all.to_df()
+        self.adata.obs.insert(loc=3, column='new_leiden', value=0)
+        for i in adList_obs.keys():
+            for k in adList_obs[i].index:
+                self.adata.obs.loc[k,'new_leiden'] = i+"_"+adList_obs[i].loc[k,'leiden']  
+        self.ad_all = ad_all
+        
+    def get_supercell_exp(self,pvalue_matrix):
+        super_exp_tra = self.ad_all.loc[pvalue_matrix.index,pvalue_matrix.columns]
+        super_exp_tra = self._replace_all_outliers(super_exp_tra)
+        super_exp_tra = super_exp_tra - np.min(super_exp_tra)/np.max(super_exp_tra) - np.min(super_exp_tra)
+        super_exp_tra = super_exp_tra.replace(np.nan,0)
+        self.super_exp_tra = super_exp_tra       
+
+class SCRIPro_Multiome():
+    def __init__(self,cores,species,Ori_Data):
+        self.cores = cores
+        self.Ori_Data = Ori_Data
+        self.species = species
+    def cal_ISD_parallel(self, bw_path):
+        lisa_test = FromGenes(self.species, rp_map='enhanced_10K', assays=['Direct'], isd_method='chipseq', verbose=1)
+        datainterface = lisa_test.data_interface
+        rpmap_enhanced = datainterface.get_rp_map('enhanced_10K')
+        factor_binging, factor_dataset_ids, factor_metadata = datainterface.get_binding_data(technology='ChIP-seq')
+        final_dict = {}
+        lisa_info = {}
+        for i in self.Ori_Data.marker_list.keys():
+            query_list = self.Ori_Data.marker_list[i]
+            query_genes, background_genes = lisa_test._get_query_and_background_genes(query_list)
+            gene_mask, label_vector, gene_info_dict = lisa_test._make_gene_mask(query_genes, background_genes)
+            lisa_info[i] = [gene_mask, label_vector, gene_info_dict,self.species]
+        with Pool(processes=self.cores) as pool:
+            results = pool.starmap(process_marker, [(i, lisa_info, bw_path,rpmap_enhanced,factor_binging,factor_metadata,self.species) for i in lisa_info.keys()])
+            pool.close()
+            pool.join()
+    
+        for i, factor_metadata_pd in results:
+            final_dict[i] = factor_metadata_pd
+    
+        self.results = final_dict
+
+    def get_P_value_matrix(self):
+        results_combine = []
+        for i in self.results.keys():
+            test = self.results[i].sort_values(by = 'p_vals').drop_duplicates(subset='factor', keep='first').loc[:,['factor','p_vals']]
+            test[i] = -np.log(test['p_vals'])
+            test.index = test['factor']
+            test = test.loc[:,i]
+            results_combine.append(test)
+        all_result_combine = pd.concat([i for i in results_combine],axis=1)
+        all_result_combine = (all_result_combine - all_result_combine.mean())/(all_result_combine.std())
+        all_result_combine.clip(upper=5, inplace=True)
+        all_result_combine = (all_result_combine - all_result_combine.min())/(all_result_combine.max() - all_result_combine.min())
+        all_result_combine = all_result_combine.T
+        P_value_matrix = all_result_combine
+        self.P_value_matrix = P_value_matrix
+    
+    def get_chip_matrix(self):
+        results_combine = []
+        for i in self.results.keys():
+            test = self.results[i].sort_values(by = 'p_vals').drop_duplicates(subset='factor', keep='first').loc[:,['sample_id','factor']]
+            test.index = test['factor']
+            test=test.iloc[:,0]
+            test.name = i
+            results_combine.append(test)
+        all_result_combine = pd.concat([i for i in results_combine],axis=1)
+        all_result_combine = all_result_combine.T
+        chip_matrix = all_result_combine
+        self.chip_matrix = chip_matrix
+    
+    
+    def get_tf(self,target_h5):
+        super_gene_exp = self.Ori_Data.super_gene_exp
+        super_gene_mean = self.Ori_Data.super_gene_mean
+        super_gene_std = self.Ori_Data.super_gene_std
+        chip_matrix2 = self.chip_matrix.copy()
+        chip_matrix2 = chip_matrix2.loc[:,list(set(chip_matrix2.columns).intersection(set(super_gene_exp.columns)))]
+        
+        querygene_dict = get_quert_gene(chip_matrix2,target_h5,super_gene_exp.columns)
+        chip_matrix2 = chip_matrix2.applymap(lambda x: querygene_dict[x])
+        
+        chip_matrix2=chip_matrix2.reset_index().rename(columns={'index': 'row'})
+        chip_matrix2_melt = chip_matrix2.melt(id_vars='row', var_name='column', value_name='value')
+        self.chip_matrix_melt = chip_matrix2_melt
+        def cal_tf_score(row):
+        # 这里的row是一个Series，它的index是['column', 'value']
+            tf_name = row['column']
+            supercell_name = row['row']
+            query_gene = row['value']
+            return ((super_gene_exp.loc[supercell_name, query_gene] - super_gene_mean[query_gene])/super_gene_std[query_gene]).mean() + (super_gene_exp.loc[supercell_name, tf_name] - super_gene_mean[tf_name])/ super_gene_std[tf_name]
+        
+        
+        chip_matrix2_melt['value2'] = chip_matrix2_melt.apply(cal_tf_score, axis=1)
+        chip_matrix2_new = chip_matrix2_melt.pivot(index='row', columns='column', values='value2')
+        # pivot()函数会创建一个MultiIndex，你可以使用reset_index()函数将行索引转换回普通列
+        chip_matrix2_new = chip_matrix2_new.reset_index()
+        # 使用rename_axis()函数删除列索引名称
+        chip_matrix2_new= chip_matrix2_new.rename_axis(None, axis=1)
+        chip_matrix2_new = chip_matrix2_new.set_index('row')
+        
+        chip_matrix2_new.clip(upper=4,lower=-4,inplace=True)
+        chip_matrix2_new = (chip_matrix2_new - chip_matrix2_new.min())/(chip_matrix2_new.max() - chip_matrix2_new.min())
+        chip_matrix2_new = chip_matrix2_new.replace(np.nan,0)
+        
+        self.P_value_matrix = self.P_value_matrix.loc[chip_matrix2_new.index,chip_matrix2_new.columns]
+        test_mat = chip_matrix2_new.mul(self.P_value_matrix)
+        
+        
+        self.tf_score = test_mat
+    
+    def get_tf_target(self,TF):
+        C =self.chip_matrix_melt[self.chip_matrix_melt.column == TF]
+        merged = pd.merge(self.Ori_Data.adata.obs, self.Ori_Data.adata.raw.to_adata().to_df(), left_index=True, right_index=True)
+        results = []
+        for index, row in C.iterrows():
+            cluster = row['row']
+            target_genes = row['value']
+            cluster_data = merged[merged['new_leiden'] == cluster]
+            tf_results = pd.DataFrame(index=[cluster], columns=target_genes)
+        
+        # 计算tf和每个target gene的相关性
+            for gene in target_genes:
+                tf_expression = cluster_data[TF]
+                gene_expression = cluster_data[gene]
+                corr, _ = pearsonr(tf_expression, gene_expression)
+                tf_results.loc[cluster, gene] = corr
+        
+        # 保存结果
+            results.append(tf_results*self.tf_score.loc[cluster,TF])
+        return pd.concat(results).clip(lower=0).replace(np.nan,0)
+        
+    
+        
+        
+        
+        
+class SCRIPro_RNA(SCRIPro_Multiome):  # 定义派生类
+    def __init__(self, cores, species, Ori_Data, assays):
+        super().__init__(cores, species, Ori_Data)  # 使用super()调用基类的构造方法
+        self.assays = assays
+        
+    def _chunks(self,data):
+        SIZE=math.ceil(len(data)/self.cores)
+        it = iter(data)
+        for i in range(0, len(data), SIZE):
+            yield {k:data[k] for k in islice(it, SIZE)}
+    
+    def cal_ISD_cistrome(self):
+        input_table_split = [item for item in self._chunks(self.Ori_Data.marker_list)]
+        pool = mp.Pool(self.cores)
+        cal = partial(cal_tf,species1=self.species, assays1=self.assays)
+        results = pool.map(cal,[row for row in input_table_split])
+        self.results = {k:v for d in results for k,v in d.items()}
+            
+    def get_P_value_matrix(self):
+        results_combine = []
+        for i in self.results.keys():
+            test = self.results[i].sort_values(by = 'summary_p_value').drop_duplicates(subset='factor', keep='first').loc[:,['factor','summary_p_value']]
+            test[i] = -np.log(test['summary_p_value'])
+            test.index = test['factor']
+            test = test.loc[:,i]
+            results_combine.append(test)
+        all_result_combine = pd.concat([i for i in results_combine],axis=1)
+        all_result_combine = (all_result_combine - all_result_combine.mean())/(all_result_combine.std())
+        all_result_combine.clip(upper=5, inplace=True)
+        all_result_combine = (all_result_combine - all_result_combine.min())/(all_result_combine.max() - all_result_combine.min())
+        all_result_combine = all_result_combine.T
+        P_value_matrix = all_result_combine
+        self.P_value_matrix = P_value_matrix 
+    
+    def get_chip_matrix(self):
+        results_combine = []
+        for i in self.results.keys():
+            test = self.results[i].sort_values(by = 'summary_p_value').drop_duplicates(subset='factor', keep='first').loc[:,['sample_id','factor']]
+            test.index = test['factor']
+            test=test.iloc[:,0]
+            test.name = i
+            results_combine.append(test)
+        all_result_combine = pd.concat([i for i in results_combine],axis=1)
+        all_result_combine = all_result_combine.T
+        chip_matrix = all_result_combine
+        self.chip_matrix = chip_matrix
+    
+        
+
+          
+
---
+++ scrippro/utils.py
@@ -0,0 +1,249 @@
+import math
+import random
+import os
+import warnings
+from collections import Counter
+from multiprocessing import Pool
+from itertools import islice
+import anndata as ad
+import math
+import h5py
+import shutil
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import subprocess
+import scanpy as sc
+import multiprocessing
+import seaborn as sns
+from lisa import FromCoverage, FromGenes, FromRegions
+from lisa.core.assays import LISA_RP_Assay, delta_RP_wrapper, transform_RP,get_delta_RP,get_deltaRP_activation
+from supercell import *
+from utils import *
+from functools import partial
+from scipy import stats, sparse
+from scipy.stats import kendalltau, pearsonr, spearmanr
+
+def write_dataframe_to_tsv(df_subset, filename):
+    # 将DataFrame子集转换为Series对象
+    series = df_subset.stack().reset_index()
+    series = series[series.iloc[:, 2] != 0]
+    # 打开文件并写入数据
+    with open(filename, 'w') as tsvfile:
+        for _, row in series.iterrows():
+            barcode = row['barcode']
+            position_parts = row['level_1'].split('_')
+            chr_name = position_parts[0]
+            start_pos = position_parts[1]
+            end_pos = position_parts[2]
+            open_value = row[0]
+
+            # 写入数据到文件
+            tsvfile.write(f"{chr_name}\t{start_pos}\t{end_pos}\t{barcode}\t{open_value}\n")
+            
+
+def get_marker_for_group(adata, i, log2fc, pval, gene_list_len):
+    tem_gene = list(sc.get.rank_genes_groups_df(adata, group=i, log2fc_min=log2fc, pval_cutoff=pval).sort_values(by = 'logfoldchanges',ascending = False)[0:500].names)
+    if len(tem_gene) < gene_list_len:
+        return i, None
+    else:
+        return i, tem_gene
+
+def process_and_merge(df, folder_path, n_num=20):
+    # 数据集分割份数和最大进程数
+    num_partitions = min(n_num, len(df))  # 分割份数不超过DataFrame的长度
+    dfs = np.array_split(df, num_partitions)
+
+    # 创建进程池
+    pool = multiprocessing.Pool(processes=num_partitions)
+
+    # 利用进程池并行写入数据
+    for i, df_subset in enumerate(dfs):
+        filename = f"{folder_path}/output_{i}.tsv"
+        pool.apply_async(write_dataframe_to_tsv, args=(df_subset, filename))
+
+    # 等待所有进程完成
+    pool.close()
+    pool.join()
+
+    # 合并并删除文件
+    merge_and_delete_tsv(folder_path)
+
+def merge_and_delete_tsv(folder_path):
+    # 使用Linux命令合并所有.tsv文件
+    subprocess.run(f'cat {folder_path}/*.tsv > {folder_path}/merge.tsv', shell=True)
+    # 使用Linux命令删除原始.tsv文件
+    subprocess.run(f'rm {folder_path}/output*.tsv', shell=True)
+
+
+def cal_tf(input_mat,species1, assays1):
+    all_tf_result = {}
+    lisa_test2 = FromGenes(species=species1,rp_map='enhanced_10K', assays=assays1,isd_method='chipseq', verbose = 0)  
+    for k,v in input_mat.items():
+        results, metadata = lisa_test2.predict(v, num_background_genes = 3000, background_strategy = 'regulatory')
+        results = pd.DataFrame(results.to_dict())
+        all_tf_result[k] = results
+    return all_tf_result 
+
+
+def process_group(group,adata,log,pval):
+    return group, list(sc.get.rank_genes_groups_df(adata, group=group, log2fc_min=log, pval_cutoff=pval).sort_values(by='logfoldchanges', ascending=False)[0:500].names)
+
+def sort_tsv(working_directory):
+    command = 'ls ./*.tsv | xargs -I {} sh -c \'bedtools sort -i "$1" > ./sort_tsv/"$(basename -- "$1")"\' -- {}'
+    folder_name = os.path.join(working_directory, "sort_tsv")
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    else:
+        print("文件夹已经存在：", folder_name)
+    print(folder_name)
+    p = subprocess.Popen(command, cwd=working_directory, shell=True)
+    p.wait()
+    
+def merge_tsv(working_directory):
+    command = 'ls *.tsv | xargs -P 4 -n 1 -I {} sh -c \'bedtools merge -d 1000 -c 4 -o sum -i "{}" > "./merge_tsv/{}"\''
+    folder_name = os.path.join(working_directory, "merge_tsv")
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    else:
+        print("文件夹已经存在：", folder_name)
+    print(folder_name)
+    p = subprocess.Popen(command, cwd=working_directory, shell=True)
+    p.wait()
+    
+def get_bigwig(working_directory):
+    command = 'find . -name \'*.tsv\' -type f -print0 | xargs -0 -P 4 -n 1 sh -c \'file=\"$1\"; ~/data/bedGraphToBigWig \"$file\" ~/data/hg38.genome \"./bigwig/${file%.*}.bw\"\' sh'
+    folder_name = os.path.join(working_directory, "bigwig")
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    else:
+        print("文件夹已经存在：", folder_name)
+    p = subprocess.Popen(command, cwd=working_directory, shell=True)
+    p.wait()
+
+def get_supercell_fragment(leiden_clusters,base_dir,fragment_file,chunksize = 10000000):
+    folder_name = base_dir+"/supercell_fragment"
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    else:
+        print("文件夹已经存在：", folder_name)
+    for i,chunk in enumerate(pd.read_csv(fragment_file, delimiter='\t',comment='#', chunksize=chunksize,header=None, names=['chrom', 'start', 'end', 'cell', 'fragment'])):
+        chunk.set_index('cell', inplace=True)
+        groups = chunk.groupby(leiden_clusters['new_leiden'])
+        for group_name, group_data in groups:
+            file_path = os.path.join(base_dir, 'supercell_fragment', f'{group_name}.tsv')
+            group_data = group_data[group_data['chrom'].str.startswith('chr')]
+            group_data.to_csv(file_path, sep='\t',mode='a', header=False, index=False)
+        print(f"Processed chunk {i+1}")
+    print('final')
+
+def process_tsv(working_directory, species):
+    # sort operation
+    sort_command = 'ls ./*.tsv | xargs -I {} sh -c \'bedtools sort -i "$1" > ./sort_tsv/"$(basename -- "$1")"\' -- {}'
+    sort_folder = os.path.join(working_directory, "sort_tsv")
+    if not os.path.exists(sort_folder):
+        os.mkdir(sort_folder)
+    p = subprocess.Popen(sort_command, cwd=working_directory, shell=True)
+    p.wait()
+
+    # merge operation
+    merge_command = 'ls *.tsv | xargs -P 4 -n 1 -I {} sh -c \'bedtools merge -d 1000 -c 4 -o sum -i "{}" > "./merge_tsv/{}"\''
+    merge_folder = os.path.join(sort_folder, "merge_tsv")
+    if not os.path.exists(merge_folder):
+        os.mkdir(merge_folder)
+    p = subprocess.Popen(merge_command, cwd=sort_folder, shell=True)
+    p.wait()
+
+    # bigwig operation
+    bigwig_command = 'find . -name \'*.tsv\' -type f -print0 | xargs -0 -P 4 -n 1 sh -c \'file=\"$1\"; ~/data/bedGraphToBigWig \"$file\" ~/data/{0}.genome \"./bigwig/${{file%.*}}.bw\"\' sh'.format(species)
+    bigwig_folder = os.path.join(merge_folder, "bigwig")
+    if not os.path.exists(bigwig_folder):
+        os.mkdir(bigwig_folder)
+    p = subprocess.Popen(bigwig_command, cwd=merge_folder, shell=True)
+    p.wait()
+
+    # move bigwig folder to current directory and remove supercell_fragment folder
+    shutil.move(bigwig_folder, os.path.join(os.getcwd(), "bigwig"))
+    shutil.rmtree(working_directory)
+    
+def process_marker(i,lisa_info,bw_path,rpmap_enhanced,factor_binging,factor_metadata,species):
+    cell_info = lisa_info[i]
+    gene_mask = cell_info[0]
+    label_vector = cell_info[1]
+    bigwig_path = bw_path + str(i) + '.bw'
+    testbw = FromCoverage.convert_bigwig(bigwig_path, species)
+
+    test_profile = testbw[:, np.newaxis]
+    test_profile = test_profile / test_profile.sum() * 1e5
+     
+    rp_matrix = rpmap_enhanced.dot(test_profile)
+    subset_rp_matrix = rp_matrix[gene_mask, :]
+    
+    bin_mask = np.squeeze(np.array(rpmap_enhanced[gene_mask, :].tocsc().sum(axis=0) > 0))
+    
+    subset_factor_binding = factor_binging[bin_mask, :]
+    subset_rp_map = rpmap_enhanced[gene_mask, :][:, bin_mask]
+    subset_accessibility = test_profile[bin_mask, :]
+    
+    rp_knockout = get_delta_RP(subset_accessibility, subset_factor_binding, subset_rp_map)[:, np.newaxis, :]
+    deltaX = get_deltaRP_activation(subset_rp_matrix[:, :, np.newaxis], rp_knockout)
+    p_vals = get_delta_RP_p_value(deltaX.transpose(0, 2, 1)[:, :, 0], label_vector)
+    factor_metadata_pd = pd.DataFrame(factor_metadata)
+    factor_metadata_pd['p_vals'] = p_vals
+    return i, factor_metadata_pd
+
+
+def glue_supercell(combined):
+    for i in set(combined.obs.leiden):
+        leiden_index = combined.obs.loc[combined.obs.leiden == i].index
+        sub_test = combined[combined.obs.leiden == i]
+        get_leiden_based_on_ncell(sub_test,50,verbose=False)
+        if sub_test.obs.leiden.value_counts()[0]>30 and sub_test.obs.leiden.value_counts().shape[0]>2:
+            clusters = sub_test.obs.leiden.cat.categories
+            centroids = pd.DataFrame({cluster: np.mean(sub_test[sub_test.obs.index[np.where(sub_test.obs.leiden == cluster)]].obsm['X_glue'],axis=0) for cluster in clusters}).T
+            small_clusters = sub_test.obs.leiden.value_counts()[sub_test.obs.leiden.value_counts() < 30].index
+            replace_dict= {}
+            for cluster in small_clusters:
+                distances = cdist(centroids.loc[[cluster]], centroids)
+                distances = distances[0][0:len(clusters)-len(small_clusters)]
+                nearest_cluster = clusters[np.argmin(distances)]
+                replace_dict[cluster] = nearest_cluster
+            sub_test.obs.leiden = sub_test.obs.leiden.replace(replace_dict)
+            sub_test.obs['leiden'] = i + "_" + sub_test.obs['leiden'].astype(str)
+        else:
+            sub_test.obs['leiden'] = i + "_0"
+        combined.obs.loc[leiden_index,'new_leiden'] = sub_test.obs['leiden']
+def get_delta_RP_p_value(gene_TF_scores, label_vector):
+        '''
+        gene_TF_scores: gene x TF, model output of delta-RP matrix. more purturbation of genes of interest correspond with higher delta regulation score
+        '''
+        #seperate matrix into query and background sets
+        query_delta = gene_TF_scores[label_vector.astype(bool)]
+        background_delta = gene_TF_scores[~label_vector.astype(bool)]
+
+        #for each TF, calculate p-value of difference in delta-R distributions between query and background genes
+        test_parameters = list(zip(query_delta.T, background_delta.T))
+
+        p_vals = [
+            mannu_test_function((q,b)) for q,b in test_parameters
+        ]
+
+        _, p_values = list(zip(*p_vals))
+
+        return p_values
+    
+def mannu_test_function(x):
+    query, background = x
+    try:
+        return stats.mannwhitneyu(query, background, alternative = 'greater')
+    #if all values in query and background are equal (no knockouts), throws value error
+    except ValueError:
+        #catch, return none for test statistic, 1.0 for p-val
+        return (None, 1.0)
+
+def get_quert_gene(chip_matrix,target_h5,gene_name):
+    tem = {}
+    for i in set(chip_matrix.values.flatten()):
+        tem[i]=list(set(target_h5[i][target_h5[i]>2].index).intersection(set(gene_name)))
+    return tem
