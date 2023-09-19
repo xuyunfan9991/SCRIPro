f068a5505a45bab3aeaecd2df9188f9759c8fdcf
xuyunfan9991 <xuyunfan9991@gmail.com>
2023-09-19 13:00:23 +0800

    Modify some code and package scripro

--- SCRIPpro/utils.py
+++ SCRIPpro/utils.py
@@ -19,17 +19,13 @@ import multiprocessing
 import seaborn as sns
 from lisa import FromCoverage, FromGenes, FromRegions
 from lisa.core.assays import LISA_RP_Assay, delta_RP_wrapper, transform_RP,get_delta_RP,get_deltaRP_activation
-from supercell import *
-from utils import *
 from functools import partial
 from scipy import stats, sparse
 from scipy.stats import kendalltau, pearsonr, spearmanr
 
 def write_dataframe_to_tsv(df_subset, filename):
-    # 将DataFrame子集转换为Series对象
     series = df_subset.stack().reset_index()
     series = series[series.iloc[:, 2] != 0]
-    # 打开文件并写入数据
     with open(filename, 'w') as tsvfile:
         for _, row in series.iterrows():
             barcode = row['barcode']
@@ -38,8 +34,6 @@ def write_dataframe_to_tsv(df_subset, filename):
             start_pos = position_parts[1]
             end_pos = position_parts[2]
             open_value = row[0]
-
-            # 写入数据到文件
             tsvfile.write(f"{chr_name}\t{start_pos}\t{end_pos}\t{barcode}\t{open_value}\n")
             
 
@@ -51,29 +45,26 @@ def get_marker_for_group(adata, i, log2fc, pval, gene_list_len):
         return i, tem_gene
 
 def process_and_merge(df, folder_path, n_num=20):
-    # 数据集分割份数和最大进程数
-    num_partitions = min(n_num, len(df))  # 分割份数不超过DataFrame的长度
+    num_partitions = min(n_num, len(df))
     dfs = np.array_split(df, num_partitions)
-
-    # 创建进程池
     pool = multiprocessing.Pool(processes=num_partitions)
 
-    # 利用进程池并行写入数据
+    
     for i, df_subset in enumerate(dfs):
         filename = f"{folder_path}/output_{i}.tsv"
         pool.apply_async(write_dataframe_to_tsv, args=(df_subset, filename))
 
-    # 等待所有进程完成
+    
     pool.close()
     pool.join()
 
-    # 合并并删除文件
+    
     merge_and_delete_tsv(folder_path)
 
 def merge_and_delete_tsv(folder_path):
-    # 使用Linux命令合并所有.tsv文件
+    
     subprocess.run(f'cat {folder_path}/*.tsv > {folder_path}/merge.tsv', shell=True)
-    # 使用Linux命令删除原始.tsv文件
+    
     subprocess.run(f'rm {folder_path}/output*.tsv', shell=True)
 
 
@@ -107,7 +98,7 @@ def get_supercell_fragment(leiden_clusters,base_dir,fragment_file,chunksize = 10
     print('final')
 
 def process_tsv(working_directory, bedGraph,species):
-    # sort operation
+    
     sort_command = 'ls ./*.tsv | xargs -I {} sh -c \'bedtools sort -i "$1" > ./sort_tsv/"$(basename -- "$1")"\' -- {}'
     sort_folder = os.path.join(working_directory, "sort_tsv")
     if not os.path.exists(sort_folder):
@@ -115,7 +106,7 @@ def process_tsv(working_directory, bedGraph,species):
     p = subprocess.Popen(sort_command, cwd=working_directory, shell=True)
     p.wait()
 
-    # merge operation
+    
     merge_command = 'ls *.tsv | xargs -P 4 -n 1 -I {} sh -c \'bedtools merge -d 1000 -c 4 -o sum -i "{}" > "./merge_tsv/{}"\''
     merge_folder = os.path.join(sort_folder, "merge_tsv")
     if not os.path.exists(merge_folder):
@@ -123,7 +114,7 @@ def process_tsv(working_directory, bedGraph,species):
     p = subprocess.Popen(merge_command, cwd=sort_folder, shell=True)
     p.wait()
 
-    # bigwig operation
+    
     bigwig_command = 'find . -name \'*.tsv\' -type f -print0 | xargs -0 -P 4 -n 1 sh -c \'file=\"$1\"; {0} \"$file\" {1} \"./bigwig/${{file%.*}}.bw\"\' sh'.format(bedGraph,species)
     bigwig_folder = os.path.join(merge_folder, "bigwig")
     if not os.path.exists(bigwig_folder):
@@ -131,7 +122,7 @@ def process_tsv(working_directory, bedGraph,species):
     p = subprocess.Popen(bigwig_command, cwd=merge_folder, shell=True)
     p.wait()
 
-    # move bigwig folder to current directory and remove supercell_fragment folder
+    
     shutil.move(bigwig_folder, os.path.join(os.getcwd(), "bigwig"))
     shutil.rmtree(working_directory)
     
@@ -186,11 +177,11 @@ def get_delta_RP_p_value(gene_TF_scores, label_vector):
         '''
         gene_TF_scores: gene x TF, model output of delta-RP matrix. more purturbation of genes of interest correspond with higher delta regulation score
         '''
-        #seperate matrix into query and background sets
+        
         query_delta = gene_TF_scores[label_vector.astype(bool)]
         background_delta = gene_TF_scores[~label_vector.astype(bool)]
 
-        #for each TF, calculate p-value of difference in delta-R distributions between query and background genes
+        
         test_parameters = list(zip(query_delta.T, background_delta.T))
 
         p_vals = [
@@ -205,9 +196,9 @@ def mannu_test_function(x):
     query, background = x
     try:
         return stats.mannwhitneyu(query, background, alternative = 'greater')
-    #if all values in query and background are equal (no knockouts), throws value error
+    
     except ValueError:
-        #catch, return none for test statistic, 1.0 for p-val
+        
         return (None, 1.0)
 
 def get_quert_gene(chip_matrix,target_h5,gene_name):
@@ -215,3 +206,41 @@ def get_quert_gene(chip_matrix,target_h5,gene_name):
     for i in set(chip_matrix.values.flatten()):
         tem[i]=list(set(target_h5[i][target_h5[i]>2].index).intersection(set(gene_name)))
     return tem
+
+def maxmin(dataframe):
+    return (dataframe - dataframe.min())/(dataframe.max() - dataframe.min())
+
+def get_cell_barcode_score(super_leiden,super_leiden_value,adList_obs):
+    s = []
+    for i in super_leiden:
+        origin_leiden = adList_obs[i.split(sep = '_')[0]]
+        num = len(origin_leiden.loc[origin_leiden.leiden ==i.split(sep = '_')[1]].index)
+        s.extend(list(zip([pd.DataFrame(super_leiden_value).loc[i,:]]*num,origin_leiden.loc[origin_leiden.leiden ==i.split(sep = '_')[1]].index)))
+    return s
+
+def plot_spatial(TF, TF_matrix, adList_obs, adata, spot_size, fig_path):
+    plot_basis='spatial'
+    s = get_cell_barcode_score(TF_matrix.loc[:,TF].index,TF_matrix.loc[:,TF],adList_obs)
+    plt.figure(figsize=(10, 7), dpi=80)
+    f, ax = plt.subplots()
+
+    spot = sorted(s,key = lambda x: x[0][0],reverse=False)
+
+    init_points = []
+    value = []
+    for _, cell in spot:
+        init_points.append(cell)
+        value.append(_)
+        
+    points = ax.scatter(adata[init_points].obsm[plot_basis][:, 0], adata[init_points].obsm[plot_basis][:, 1], c=value, s = spot_size,cmap="PuRd",edgecolors = None)
+    plt.scatter(adata[init_points].obsm[plot_basis][:, 0], adata[init_points].obsm[plot_basis][:, 1], c=value, s = spot_size,cmap="PuRd",edgecolors = None)
+    f.colorbar(points)
+    ax.grid(False)
+    # # ax = plt.gca()
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    plt.savefig(fig_path)
